2147facfef249c6df98384062570a45c1c3e795e
Alex Chen <axc@mit.edu>
2013-05-26 17:25:05 -0400

    Removed useless files.

--- AirShare2/ServerCode/header.php
+++ AirShare2/ServerCode/header.php
@@ -4,7 +4,7 @@
 
 $start = microtime(true);
 
-$db = mysql_connect("localhost", "root", "1335238418081") or die("Failed to connect to database.");
+$db = mysql_connect("localhost", "root", "passwordhere") or die("Failed to connect to database.");
 
 mysql_select_db("airshare");
 
